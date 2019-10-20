c29c297e1cc046146ac7ec5d947193432cea7152
BILL-E_MAGNUS\Christopher <christopher.aranadi@mail.utoronto.ca>
2019-10-20 04:30:07 -0400

    kek

--- app.js
+++ app.js
@@ -3,14 +3,24 @@ const sendmail = require('sendmail')();
 const app = express();
 
 app.get('/', (req, res) => {
-    sendmail({
-        from: 'no-reply@yourdomain.com',
-        to: 'baranadi@rogers.com',
-        subject: 'test sendmail',
-        html: 'Mail of test sendmail ',
-      }, function(err, reply) {
-        res.send(reply);
+    const send = require('gmail-send')({
+        user: 'farmerbob595@gmail.com',
+        pass: 'bobfarm123',
+        to:   'christopher.aranadi@gmail.com',
+        subject: 'test subject',
+        html: 'this value'
     });
+
+    send({
+        text: 'gmail-send example 1',  
+    }, (error, result, fullResult) => {
+        if (error) {
+            console.error(error);
+        } 
+        else {
+            console.log(result);
+        }
+    })
 });
 
 app.listen(3001, () => {
