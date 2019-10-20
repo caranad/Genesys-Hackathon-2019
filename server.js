fbe4f7367474ef1ffbc17e41b5bc9d4b83bd3e8a
TheInvoker <rydsouza82@gmail.com>
2019-10-20 12:02:09 -0400

    update

--- server.js
+++ server.js
@@ -200,24 +200,65 @@ function hexToRgb(hex) {
     } : null;
   }
 
+  function deltaE(rgbA, rgbB) {
+    let labA = rgb2lab(rgbA);
+    let labB = rgb2lab(rgbB);
+    let deltaL = labA[0] - labB[0];
+    let deltaA = labA[1] - labB[1];
+    let deltaB = labA[2] - labB[2];
+    let c1 = Math.sqrt(labA[1] * labA[1] + labA[2] * labA[2]);
+    let c2 = Math.sqrt(labB[1] * labB[1] + labB[2] * labB[2]);
+    let deltaC = c1 - c2;
+    let deltaH = deltaA * deltaA + deltaB * deltaB - deltaC * deltaC;
+    deltaH = deltaH < 0 ? 0 : Math.sqrt(deltaH);
+    let sc = 1.0 + 0.045 * c1;
+    let sh = 1.0 + 0.015 * c1;
+    let deltaLKlsl = deltaL / (1.0);
+    let deltaCkcsc = deltaC / (sc);
+    let deltaHkhsh = deltaH / (sh);
+    let i = deltaLKlsl * deltaLKlsl + deltaCkcsc * deltaCkcsc + deltaHkhsh * deltaHkhsh;
+    return i < 0 ? 0 : Math.sqrt(i);
+  }
+  
+  function rgb2lab(rgb){
+    let r = rgb[0] / 255, g = rgb[1] / 255, b = rgb[2] / 255, x, y, z;
+    r = (r > 0.04045) ? Math.pow((r + 0.055) / 1.055, 2.4) : r / 12.92;
+    g = (g > 0.04045) ? Math.pow((g + 0.055) / 1.055, 2.4) : g / 12.92;
+    b = (b > 0.04045) ? Math.pow((b + 0.055) / 1.055, 2.4) : b / 12.92;
+    x = (r * 0.4124 + g * 0.3576 + b * 0.1805) / 0.95047;
+    y = (r * 0.2126 + g * 0.7152 + b * 0.0722) / 1.00000;
+    z = (r * 0.0193 + g * 0.1192 + b * 0.9505) / 1.08883;
+    x = (x > 0.008856) ? Math.pow(x, 1/3) : (7.787 * x) + 16/116;
+    y = (y > 0.008856) ? Math.pow(y, 1/3) : (7.787 * y) + 16/116;
+    z = (z > 0.008856) ? Math.pow(z, 1/3) : (7.787 * z) + 16/116;
+    return [(116 * y) - 16, 500 * (x - y), 200 * (y - z)]
+  }
+
 function askGenesysQuestion(token, env_data, callback) {
 
     // transformation
-    if (env_data.temperature < 10) { var temp = 'cold'; }
-    else if (env_data.temperature < 20) { var temp = 'mild'; }
+    if (env_data.temperature < 20) { var temp = 'cold'; }
+    //else if (env_data.temperature < 20) { var temp = 'mild'; }
     else { var temp = 'hot'; }
 
-    if (env_data.weather.indexOf("part") !== -1) { var weather = 'mild'; }
-    else if (env_data.weather.indexOf("mist") !== -1) { var weather = 'no rain'; }
-    else if (env_data.weather.indexOf("cloud") !== -1) { var weather = 'rain'; }
+    // no rain, mist, clear, rain, partly cloudy
+    if (env_data.weather.indexOf("clear sky") !== -1) { var weather = 'no rain'; }
+    else if (env_data.weather.indexOf("few clouds") !== -1) { var weather = 'no rain'; }
+    else if (env_data.weather.indexOf("scattered clouds") !== -1) { var weather = 'no rain'; }
+    else if (env_data.weather.indexOf("broken clouds") !== -1) { var weather = 'no rain'; }
+    else if (env_data.weather.indexOf("shower rain") !== -1) { var weather = 'rain'; }
+    else if (env_data.weather.indexOf("light rain") !== -1) { var weather = 'rain'; }
     else if (env_data.weather.indexOf("rain") !== -1) { var weather = 'rain'; }
-    else if (env_data.weather.indexOf("fog") !== -1) { var weather = 'mild'; }
-    else { var weather = 'sunny'; }
+    else if (env_data.weather.indexOf("thunderstorm") !== -1) { var weather = 'rain'; }
+    else if (env_data.weather.indexOf("snow") !== -1) { var weather = 'rain'; }
+    else if (env_data.weather.indexOf("mist") !== -1) { var weather = 'no rain'; }
+    else if (env_data.weather.indexOf("sunny") !== -1) { var weather = 'no rain'; }
+    else { var weather = ""; }
 
     var rgb = hexToRgb(env_data.img_data.color);
-    var cd_green = colorDistance([rgb.r, rgb.g, rgb.b], [9, 214, 50]); // green
-    var cd_yellow = colorDistance([rgb.r, rgb.g, rgb.b], [255, 208, 0]); // yellow
-    var cd_brown = colorDistance([rgb.r, rgb.g, rgb.b], [145, 101, 58]); // brown
+    var cd_green = deltaE([rgb.r, rgb.g, rgb.b], [69, 96, 39]); // green
+    var cd_yellow = deltaE([rgb.r, rgb.g, rgb.b], [255, 208, 0]); // yellow
+    var cd_brown = deltaE([rgb.r, rgb.g, rgb.b], [82, 40, 0]); // brown
     var d = [{c:'green',v:cd_green}, {c:'yellow',v:cd_yellow}, {c:'brown',v:cd_brown}];
     //console.log(d);
     d = d.sort(function(a,b) {
@@ -230,14 +271,14 @@ function askGenesysQuestion(token, env_data, callback) {
         return ["insect", "bug", "bee", "wasp", "ladybug", "ant", "beetle", "fly", "butterfly", "caterpillar", "centipede", "moth", "cocoon", "larva", "larvae", "worm", "locust"].indexOf(x) !== -1;
     }).length > 0 ? "yes" : "no";
 
-    var QUES = "Location: " + env_data.city + "; Color: " + color + "; Pests: " + pests + "; weather forecast: " + weather + "; temperature: " + temp + "; extreme forecast: no";
+    var QUES = /*"Location: " + env_data.city + "; */"Color: " + color + "; Pests: " + pests + "; weather forecast: " + weather + "; temperature: " + temp;// + "; extreme forecast: no";
     //QUES = "Location: Toronto; Color: green; Pests: yes; weather forecast: no rain; temperature forecast: hot; extreme forecast: no";
     console.log(QUES);
 
 
     // ask genesys
     var options = { method: 'POST',
-      url: 'https://api.genesysappliedresearch.com/v2/knowledge/knowledgebases/43a214a6-be93-477d-a2e6-25601ff1a247/search',
+      url: 'https://api.genesysappliedresearch.com/v2/knowledge/knowledgebases/eec397dd-1e93-4737-83f0-0075f67d8f5f/search',
       headers: 
        { token: token,
          organizationid: 'a078486f-4ed7-4814-af1a-11718c78ea21',
@@ -265,7 +306,7 @@ function askGenesysQuestion(token, env_data, callback) {
         //console.log(JSON.stringify(body,null,4));
 
         var results = body.results.filter(function(x) {
-            return x.confidence > 0.1;
+            return (pests === "yes" || (pests === "no" && x.faq.question.indexOf("Pests: yes;") === -1)) && x.confidence > 0.1;
         });
         if (results.length > 0) {
             var result = results[0];
